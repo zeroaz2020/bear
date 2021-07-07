efd07ad05ffc198e30fc1a6e4457002b94d62fb1
GyeCheol <godmarketing1004@gamil.com>
2021-07-07 11:16:38 +0900

    수정

--- src/components/common/userFriend/UserFriendSearch.vue
+++ src/components/common/userFriend/UserFriendSearch.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="friend__search">
-    <input
-      type="text"
-      placeholder="친구를 검색해 보세요" 
-      maxlength="30" />
+    <input type="text" placeholder="친구를 검색해 보세요" maxlength="30" />
     <div>
-      <button
-        type="button"
-        class="icon__cancel"></button>
-      <button
-        type="button"
-        class="icon__search"></button>
+      <button type="button" class="icon__cancel"></button>
+      <button type="button" class="icon__search"></button>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
+
+<style scoped lang="scss">
+.friend__search {
+  input {
+    width: 100%;
+    background: #fff;
+    box-shadow: inset 0 0 0 1px #f00;
+    border-radius: 7px;
+    font-size: 1.4rem;
+    height: 43px;
+    padding: 18px;
+    outline: none;
+  }
+}
+</style>
