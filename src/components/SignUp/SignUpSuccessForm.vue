4e141a555604d5069740fcf7d0ad483fa4ee2ee8
GyeCheol <godmarketing1004@gamil.com>
2021-07-07 10:07:00 +0900

    Update SignUpSuccessForm.vue

--- src/components/SignUp/SignUpSuccessForm.vue
+++ src/components/SignUp/SignUpSuccessForm.vue
@@ -1,29 +1,27 @@
 <template>
-  <div class="wrap">
-    <!-- 회원가입 결과 -->
-    <div class="signupsuccess__title">
-      <h1>환영합니다!</h1>
-      <ul class="signup__result">
-        <li><span>베어런</span>님, 회원가입을 축하드립니다.</li>
-        <li>아이디는 <span>master@bearun.com</span>&nbsp;입니다.</li>
-      </ul>
-
-      <p></p>
-
-      <ul class="membershipEmail__info">
-        <li>서비스 이용을 위해서 회원님의</li>
-        <li>이메일 주소 <span>master@bearun.com</span>&nbsp;로</li>
-        <li>송부 된 인증 링크를 클릭 해 주시기 바랍니다.</li>
-      </ul>
-
-      <button type="button">인증메일 다시 보내기</button>
-    </div>
-
-    <!-- Button -->
-    <ul class="footer__button">
-      <li>
-        <button type="button">시작하기</button>
-      </li>
+  <!-- 회원가입 결과 -->
+  <div class="signupsuccess__title">
+    <h1>환영합니다!</h1>
+    <ul class="signup__result">
+      <li><span>베어런</span>님, 회원가입을 축하드립니다.</li>
+      <li>아이디는 <span>master@bearun.com</span>&nbsp;입니다.</li>
     </ul>
+
+    <p></p>
+
+    <ul class="membershipEmail__info">
+      <li>서비스 이용을 위해서 회원님의</li>
+      <li>이메일 주소 <span>master@bearun.com</span>&nbsp;로</li>
+      <li>송부 된 인증 링크를 클릭 해 주시기 바랍니다.</li>
+    </ul>
+
+    <button type="button">인증메일 다시 보내기</button>
   </div>
+
+  <!-- Button -->
+  <ul class="footer__button">
+    <li>
+      <button type="button">시작하기</button>
+    </li>
+  </ul>
 </template>
